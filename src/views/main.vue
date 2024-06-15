a39f27e5d298cf6f8057dc904007b4899219a203
SONG <exhibi98@gmail.com>
2024-06-15 17:56:02 +0900

    toggle 스타일 조정

--- src/views/main.vue
+++ src/views/main.vue
@@ -31,7 +31,7 @@
         <div class="risk-label-container">
             <label class="risk-label">
                 <i class="bi bi-slash-circle-fill"></i> 리스크 설정
-                ( <span id="selectedRisk">{{ selectedRisk }}%</span> )
+                ( <span id="selectedRisk" @click="toggleTabs">{{ selectedRisk }}%</span> )
             </label>
             <label class="switch">
                 <input type="checkbox" v-model="tabsEnabled">
@@ -277,6 +277,9 @@ export default {
             this.selectedRisk = risk;
             this.riskRatio = this.selectedRisk / 100;
         },
+        toggleTabs() {
+            this.tabsEnabled = !this.tabsEnabled;
+        }
     },
     computed: {
         formattedEntryPrice() {
