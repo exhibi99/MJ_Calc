c7ea77d8fa838599b3174475ef2a1a7f38e43a5f
SONG <exhibi98@gmail.com>
2024-06-25 09:43:12 +0900

    글자크기 조정

--- src/views/main.vue
+++ src/views/main.vue
@@ -403,19 +403,4 @@ export default {
     /* 선택 사항: 밑줄 추가 */
     font-weight: bold;
 }
-
-.clipboard-button {
-    background-color: #007bff;
-    color: white;
-    border: none;
-    padding: 10px 20px;
-    font-size: 16px;
-    cursor: pointer;
-    border-radius: 5px;
-    transition: background-color 0.3s ease;
-}
-
-.clipboard-button:hover {
-    background-color: #0056b3;
-}
 </style>
